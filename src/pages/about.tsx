3ae26f21418eb672e010e9a13c6e48128e1a5b73
Ayush Manoj <ayushmanoj04@gmail.com>
2023-06-11 11:32:22 -0500

    Updated about me section

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import Links from "@/components/Links";
 import NavigationBar from "@/components/NavigationBar";
 
-const imageSource = "/images/profile.png";
-const caption = "Taking in Austin's skyline on the Congress Avenue Bridge.";
+const imageSource = "/images/mountain.png";
+const caption =
+  "Savoring the view atop Grand Teton National Park's Inspiration Point.";
 
 export default function About() {
   return (
@@ -12,31 +13,56 @@ export default function About() {
       <NavigationBar />
       <main className="flex flex-col items-center justify-evenly min-h-screen md:ml-[20rem] p-8 dark:bg-neutral-900 dark:text-white">
         <div className="w-80 p-10 border dark:shadow-none shadow-md rounded-md -rotate-[10deg] m-12 bg-white">
-          <div className="w-60 h-60 border rounded-lg relative">
-            <Image
-              className="rounded-lg"
-              alt={caption}
-              src={imageSource}
-              fill={true}
-            />
+          <div className="w-60 h-60 border relative">
+            <img className="object-contain" alt={caption} src={imageSource} />
           </div>
-          <p className="text-center text-xs dark:text-black mt-4">{caption}</p>
+          <p className="text-center text-xs dark:text-black mt-4 py-4">
+            {caption}
+          </p>
         </div>
-        <div className="max-w-4xl mx-8">
-          <h1 className="text-6xl text-center font-bold mb-6">
-            About Me
-          </h1>
-          <p>
-            I&apos;m an incoming junior at the University of Texas at Austin.
-          </p>
-          <p>
-            I&apos;m currently a computer science major but aspire to declare a
-            second major in either math or finance.
-          </p>
-          <p>
-            In my free time, I like to hit the gym, listen to all sorts of
-            music, watch movies and party with friends, eat, and sleep.
-          </p>
+        <div className="flex flex-col gap-12 max-w-4xl my-4">
+          <h1 className="text-6xl text-center font-bold">About Me</h1>
+          <div>
+            <h2 className="text-lg font-bold mb-2">Who are you?</h2>
+            <p>
+              I&apos;m an incoming junior at the University of Texas at Austin,
+              studying computer science.
+            </p>
+          </div>
+          <div>
+            <h2 className="text-lg font-bold mb-2">What do you do?</h2>
+            <ul className="list-disc list-inside">
+              <li>
+                I&apos;m passionate about building things with code. You can
+                always find me working on my latest side project, learning a new
+                tech stack or framework. The pure satisfaction I get from
+                completing a project is truly unmatched.
+              </li>
+              <li>
+                With a distinct interest in coding, mathematics, and finance, I
+                aspire to work either as a quant trader or a software engineer
+                after college.
+              </li>
+              <li>
+                Currently, I&apos;m involved in Texas Convergent, an
+                organization at UT where I work with a team on creating custom
+                software solutions for nonprofits in the Austin area.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-lg font-bold mb-2">What are your hobbies?</h2>
+            <ul className="list-disc list-inside">
+              <li>
+                In my free time, I like to hit the gym, explore different genres
+                of music, watch movies and party with friends, eat, and sleep.
+              </li>
+              <li>
+                I also really enjoy traveling and always dream of exploring new
+                places throughout my life.
+              </li>
+            </ul>
+          </div>
         </div>
       </main>
     </>
